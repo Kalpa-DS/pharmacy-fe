bba43bc99a94882298c59d17413f88781c2b48dc
Kalpa de silva <10748086@students.plymouth.ac.uk>
2023-01-22 01:30:10 +0530

    add products route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { UserRole } from './enums/user-role.enum';
+import { AuthGuard } from './guards/auth/auth.guard';
+import { RoleGuard } from './guards/role/role.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: [UserRole.Admin],
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
