25b61f9f92382f3cdab7d97e07f91511c06bcf9b
Kalpa de silva <10748086@students.plymouth.ac.uk>
2023-01-22 01:31:53 +0530

    add product item route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductItemComponent } from './components/dashboard/product-item/product-item.component';
 import { ProductComponent } from './components/product/product.component';
 import { UserRole } from './enums/user-role.enum';
 import { AuthGuard } from './guards/auth/auth.guard';
@@ -10,6 +11,14 @@ const routes: Routes = [
     path: 'products',
     component: ProductComponent,
     canActivate: [AuthGuard, RoleGuard],
+    data: {
+      roles: [UserRole.Admin],
+    },
+  },
+  {
+    path: 'dashboard/products/:productId',
+    component: ProductItemComponent,
+    canActivate: [AuthGuard],
     data: {
       roles: [UserRole.Admin],
     },
